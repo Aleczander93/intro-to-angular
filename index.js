6372b04d00e77d1345e26a359708fdc814cc7677
aleczander <afigueroa81@ufl.edu>
2016-10-25 18:49:03 -0400

    added the ability to edit and save those edits for idividual todos in the application

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ var port = process.env.PORT || 8080;
 
 server.use(express.static(__dirname + '/public'));
 
-
 server.get('/', function(req, res){
   res.sendFile('public/html/index.html', {root:__dirname});
 });
 
 server.listen(port, function(){
-  console.log('Now listening on port...', port);
+  console.log('Now listening on port...8080', port);
 });
--- public/js/controllers/main.controller.js
+++ public/js/controllers/main.controller.js
@@ -11,6 +11,8 @@
       $scope.todos = TodoService.get();
       $scope.createTodo = createTodo;
       $scope.deleteTodo = deleteTodo;
+      $scope.editTodo = editTodo;
+      $scope.saveTodo = saveTodo;
 
       function createTodo(newTodo){
         TodoService.create(newTodo);
@@ -18,6 +20,13 @@
       }
       function deleteTodo(index){
         TodoService.delete(index);
+      }
+      function editTodo(todo){
+        todo.isBeingEdited = true;
+      }
+      function saveTodo(index, todo){
+        TodoService.update(index, todo.desc);
+        todo.isBeingEdited = false;
       }
   }
 }());
